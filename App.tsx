edcc70f4b164619822cde1aeaa84ba515af20923
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-15 22:48:26 -0300

    start app

--- App.tsx
+++ App.tsx
@@ -1,65 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
 
-import mapMarker from './src/images/map-marker.png';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
+
+import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    nunito600: Nunito_600SemiBold,
+    nunito700: Nunito_700Bold,
+    nunito800: Nunito_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView 
-        provider={PROVIDER_GOOGLE}
-        style={styles.map} 
-        initialRegion={{
-          latitude: -23.5229859,
-          longitude: -46.6995142,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008
-        }}
-        >
-          <Marker 
-            icon={mapMarker}
-            calloutAnchor={{
-              x: 2.7,
-              y: 0.8,
-            }}
-            coordinate={{
-              latitude: -23.5229859,
-              longitude: -46.6995142,
-            }}
-          >
-            <Callout tooltip onPress={() => {}}>
-              <View style={styles.calloutContainer}>
-                <Text style={styles.calloutText}></Text>
-              </View>
-            </Callout>
-          </Marker>
-        </MapView>
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-
-  calloutContainer: {
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: 16,
-    justifyContent: 'center',
-  },
-
-  calloutText: {
-    color: '#0089a5',
-    fontSize: 14,
-  },
-});
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from '../images/map-marker.png';
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrphanagesMap() {
+    const navigation = useNavigation();
+
+    function handleNavigateToOrphanageDetails() {
+        navigation.navigate('OrphanageDetails');
+    }
+
+    return (
+        <View style={styles.container}>
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -23.5229859,
+          longitude: -46.6995142,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+        >
+          <Marker 
+            icon={mapMarker}
+            calloutAnchor={{
+              x: 2.7,
+              y: 0.8,
+            }}
+            coordinate={{
+              latitude: -23.5229859,
+              longitude: -46.6995142,
+            }}
+          >
+            <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}></Text>
+              </View>
+            </Callout>
+          </Marker>
+        </MapView>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+          <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+            <Feather name="plus" size={20} color='#fff'/>
+          </TouchableOpacity>
+        </View>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  
+    calloutContainer: {
+      width: 160,
+      height: 46,
+      paddingHorizontal: 16,
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      borderRadius: 16,
+      justifyContent: 'center',
+  
+      elevation: 3,
+    },
+  
+    calloutText: {
+      color: '#0089a5',
+      fontSize: 14,
+      fontFamily: 'nunito700',
+    },
+  
+    footer: {
+      position: 'absolute',
+      left: 24,
+      right: 24,
+      bottom: 32,
+  
+      backgroundColor: '#fff',
+      borderRadius: 20,
+      height: 56,
+      paddingLeft: 24,
+  
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+  
+      elevation: 3,
+    },
+  
+    footerText: {
+      fontFamily: 'nunito700',
+      color: '#8fa7b3',
+    },
+  
+    createOrphanageButton: {
+      width: 56,
+      height: 56,
+      backgroundColor: '#15c3d6',
+      borderRadius: 20,
+  
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  });
+  
