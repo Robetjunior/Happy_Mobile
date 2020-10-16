202d76465534476571dcbf004a11fae27030542a
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-15 21:57:46 -0300

    first commit mobile

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -23.5229859,
+          longitude: -46.6995142,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+        >
+          <Marker 
+            icon={mapMarker}
+            calloutAnchor={{
+              x: 2.7,
+              y: 0.8,
+            }}
+            coordinate={{
+              latitude: -23.5229859,
+              longitude: -46.6995142,
+            }}
+          >
+            <Callout tooltip onPress={() => {}}>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}></Text>
+              </View>
+            </Callout>
+          </Marker>
+        </MapView>
     </View>
   );
 }
@@ -14,8 +42,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
     justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   },
 });
